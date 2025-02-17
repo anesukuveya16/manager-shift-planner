00380bc4b212a927554eef60d5a3c4de4df2ffad
anesukuveya <anesugail16@gmail.com>
2025-02-17 23:21:39 +0100

    Project setup for manager-micrservice.

---
+++ src/main/java/com/anesu/project/managerservice/ManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anesu.project.managerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anesu/project/managerservice/ManagerServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.anesu.project.managerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
