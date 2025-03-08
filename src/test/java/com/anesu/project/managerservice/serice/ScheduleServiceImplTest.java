73ba6a1547b3dd6cd8e4e651f5b9effaf0ede584
anesukuveya <anesugail16@gmail.com>
2025-03-08 21:26:36 +0100

    WIP: ScheduleServiceImplTest

--- src/test/java/com/anesu/project/managerservice/serice/ScheduleServiceImplTest.java
+++ src/test/java/com/anesu/project/managerservice/serice/ScheduleServiceImplTest.java
@@ -12,6 +12,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -31,16 +37,34 @@ class ScheduleServiceImplTest {
     @Test
     void shouldUpdateAndSaveTheNewlyUpdatedSchedule() {
         //Given
-        Schedule schedule = new Schedule();
-        schedule.setEmployeeId(1L);
-        schedule.setStartDate(schedule.getStartDate());
-        schedule.setEndDate(LocalDateTime.from(LocalDate.now().plusDays(3).atTime(8, 30)));
-        schedule.setTotalWorkingHours(schedule.getTotalWorkingHours());
+        Schedule oldSchedule = new Schedule();
+        oldSchedule.setEmployeeId(1L);
+        oldSchedule.setStartDate(oldSchedule.getStartDate());
+        oldSchedule.setEndDate(LocalDateTime.from(LocalDate.now().plusDays(3).atTime(8, 30)));
+        oldSchedule.setTotalWorkingHours(oldSchedule.getTotalWorkingHours());
 
+        Schedule newSchedule = new Schedule();
+        newSchedule.setEmployeeId(1L);
+        newSchedule.setStartDate(oldSchedule.getStartDate());
+        newSchedule.setEndDate(LocalDateTime.from(LocalDate.now().plusDays(3).atTime(10, 45)));
+        newSchedule.setTotalWorkingHours(oldSchedule.getTotalWorkingHours());
+
+        when(scheduleRepositoryMock.findById(oldSchedule.getEmployeeId())).thenReturn(Optional.of(newSchedule));
+        doNothing().when(scheduleValidatorMock).validateSchedule(any(Schedule.class));
+        when(scheduleRepositoryMock.save(any(Schedule.class))).thenReturn(newSchedule);
 
         // When
-
+        Schedule newlyUpdatedSchedule = cut.updateEmployeeSchedule(oldSchedule.getEmployeeId(), newSchedule);
 
         // Then
+
+        assertNotNull(newlyUpdatedSchedule);
+        assertThat(newlyUpdatedSchedule.getEmployeeId()).isEqualTo(oldSchedule.getEmployeeId());
+        assertThat(newlyUpdatedSchedule.getTotalWorkingHours()).isEqualTo(newSchedule.getTotalWorkingHours());
+
+        verify(scheduleRepositoryMock, times(1)).findById(oldSchedule.getId());
+        verify(scheduleValidatorMock).validateSchedule(oldSchedule);
+        verify(scheduleRepositoryMock, times(1)).save(oldSchedule);
+
     }
 }
